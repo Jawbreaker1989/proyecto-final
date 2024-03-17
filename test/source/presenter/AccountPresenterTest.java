c3c6548f455628fa8cd7074ad0f3c4cd922217ff
julianfmesa <149394842+julianfmesa@users.noreply.github.com>
2024-03-17 12:39:45 -0500

    Update AccountPresenterTest.java

--- test/source/presenter/AccountPresenterTest.java
+++ test/source/presenter/AccountPresenterTest.java
@@ -1,65 +1,61 @@
 package presenter;
 
-import errors.HandlingErrors;
-import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class AccountPresenterTest {
+import org.junit.Before;
+import org.junit.Test;
 
+import errors.HandlingErrors;
+
+public class AccountPresenterTest {
+    
     private AccountPresenter accountPresenter;
-
-    void setup() {
-
+    
+    @Before
+    public void setUp() {
+        accountPresenter = new AccountPresenter();
     }
 
     @Test
-    void findAccount() {
+    public void testSaveAccountWithInitialBalance() {
+        try {
+            assertTrue(accountPresenter.saveAccount("123456789", "2024/03/17", 1000, AccountPresenter.CURRENT));
+        } catch (HandlingErrors e) {
+            fail("Error inesperado: " + e.getMessage());
+        }
     }
-
+    
     @Test
-    void createAccount() {
-
+    public void testFindAccount() {
+        try {
+            assertTrue(accountPresenter.saveAccount("987654321", "2024/03/17", 500, AccountPresenter.SAVINGS));
+            String[] accountInfo = accountPresenter.findAccount("987654321");
+            assertNotNull(accountInfo);
+            assertEquals("987654321", accountInfo[0]);
+        } catch (HandlingErrors e) {
+            fail("Error inesperado: " + e.getMessage());
+        }
     }
-
+    
     @Test
-    void deposit() {
-        setup();
-
+    public void testWithdrawFromAccount() {
+        try {
+            assertTrue(accountPresenter.saveAccount("777777777", "2024/03/17", 1500, AccountPresenter.CURRENT));
+            assertTrue(accountPresenter.withdraw("777777777", 500));
+        } catch (Exception e) {
+            fail("Error inesperado: " + e.getMessage());
+        }
     }
-
+    
     @Test
-    void willdraw() {
-        
-
+    public void testTransferBetweenAccounts() {
+        try {
+            assertTrue(accountPresenter.saveAccount("111111111", "2024/03/17", 2000, AccountPresenter.SAVINGS));
+            assertTrue(accountPresenter.saveAccount("222222222", "2024/03/17", 1000, AccountPresenter.CURRENT));
+            assertTrue(accountPresenter.transfer("111111111", "222222222", 500));
+        } catch (Exception e) {
+            fail("Error inesperado: " + e.getMessage());
+        }
     }
 
-    @Test
-    void transfer() {
-
-
-
-    }
-
-
-    @Test
-    void getAccounts() {
-    }
-
-    @Test
-    void averageAccount() {
-
-    }
-
-    @Test
-    void getAccountsResidueMin() {
-
-    }
-
-    @Test
-    void getMaxResidue() {
-
-    }
 }
